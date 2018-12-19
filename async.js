4363f4f487b966073e03a7fb8aa5204911ede202
Greg Price <gnprice@gmail.com>
2018-12-19 02:30:35 -0800

    Hmm, demonstrate JS is more complicated than that
    Sometimes an async function's *return* can be an opportunity for
    another event to sneak in.
    
    Maybe this is a good time to add a detail:
    
      $ node --version
      v8.14.0

--- async.js
+++ async.js
@@ -4,6 +4,14 @@ function sleep(duration) {
   return new Promise(resolve => setTimeout(resolve, duration));
 }
 
+const lines = [];
+
+function log(msg) {
+  // Possibly console.log isn't synchronous?
+  // To avoid that possible confounder, just push to a list.
+  lines.push(msg);
+}
+
 let flag = true;
 let done = false;
 
@@ -11,7 +19,7 @@ async function freq() {
   while (true) {
     if (flag) {
       flag = false;
-      console.log('in!');
+      log('in!');
     }
     if (done) {
       break;
@@ -21,22 +29,24 @@ async function freq() {
 }
 
 function say(msg) {
-  console.log(msg);
+  log(msg);
   flag = true;
 }
 
-// output: a b in! c
+// output: a b in! c d
 async function demo1() {
   say('a');
   say('b');
   await sleep(100);
   say('c');
+  say('d');
 }
 
-// output: a b in! c
+// output: a b in! c d
 async function demo2() {
   say('a');
   await demo2bc();  // Refactored into a new function.
+  say('d');
 }
 
 async function demo2bc() {
@@ -45,14 +55,48 @@ async function demo2bc() {
   say('c');
 }
 
+// output:
+// sometimes a b in! c d in!
+async function demo3() {
+  say('a');
+  say('b');
+  await sleep(0);  // Now with 0 wait.
+  say('c');
+  say('d');
+}
+
+// output:
+// sometimes a b in! c in! d
+async function demo4() {
+  say('a');
+  await demo4bc();  // Same refactoring.
+  say('d');
+}
+
+async function demo4bc() {
+  say('b');
+  await sleep(0);
+  say('c');
+}
+
 async function main() {
   freq();
-  console.log('--');
+
+  log('--');
   await demo1();
-  console.log('--');
+  log('--');
   await demo2();
-  console.log('--');
+  log('--');
+  await demo3();
+  log('--');
+  await demo4();
+  log('--');
+
   done = true;
+  await sleep(100);
+
+  const out = lines.join('\n');
+  console.log(out);
 }
 
 main();
